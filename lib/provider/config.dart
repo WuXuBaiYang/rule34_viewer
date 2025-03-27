d178ad7bb5dc107def44c91da0afb8dd7e3623c9
戊戌白羊 <wuxubaiyang@gmail.com>
2025-03-27 14:57:45 +0800

    添加全局配置管理;

--- lib/provider/config.dart
+++ lib/provider/config.dart
@@ -7,6 +7,37 @@ import 'package:jtech_base/jtech_base.dart';
 */
 class ConfigProvider extends BaseConfigProvider<AppConfig> {
   ConfigProvider(super.context) : super(creator: AppConfig.from);
+
+  // 获取标签集合
+  List<String> get tagList => config.tags;
+
+  // 添加标签
+  void addTag(String tag) =>
+      updateConfig(config.copyWith(tags: [...config.tags, tag]));
+
+  // 移除标签
+  void removeTag(String tag) =>
+      updateConfig(config.copyWith(tags: config.tags..remove(tag)));
+
+  // 移除所有标签
+  void removeAllTag() => updateConfig(config.copyWith(tags: []));
+
+  // 获取搜索记录
+  List<String> get searchHistory => config.searchHistory;
+
+  // 添加搜索记录
+  void addSearchHistory(String search) => updateConfig(
+    config.copyWith(searchHistory: [...config.searchHistory, search]),
+  );
+
+  // 移除搜索记录
+  void removeSearchHistory(String search) => updateConfig(
+    config.copyWith(searchHistory: config.searchHistory..remove(search)),
+  );
+
+  // 移除所有搜索记录
+  void removeAllSearchHistory() =>
+      updateConfig(config.copyWith(searchHistory: []));
 }
 
 /*
@@ -15,15 +46,26 @@ class ConfigProvider extends BaseConfigProvider<AppConfig> {
 * @Time 2024/8/14 14:40
 */
 class AppConfig extends BaseConfig {
-  AppConfig();
+  // 缓存过滤标签
+  final List<String> tags;
 
-  AppConfig.from(obj);
+  // 搜索记录
+  final List<String> searchHistory;
+
+  AppConfig({required this.tags, required this.searchHistory});
+
+  AppConfig.from(obj)
+    : tags = obj?['tags'] ?? [],
+      searchHistory = obj?['searchHistory'] ?? [];
 
   @override
-  Map<String, dynamic> to() => {};
+  Map<String, dynamic> to() => {'tags': tags, 'searchHistory': searchHistory};
 
   @override
-  AppConfig copyWith({String? placeholder}) {
-    return AppConfig();
+  AppConfig copyWith({List<String>? tags, List<String>? searchHistory}) {
+    return AppConfig(
+      tags: tags ?? this.tags,
+      searchHistory: searchHistory ?? this.searchHistory,
+    );
   }
 }
